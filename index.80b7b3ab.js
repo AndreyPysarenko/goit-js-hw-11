f01e104f820d98e9b9fabc348943f2dba494bf36
AndreyPysarenko <114475908+AndreyPysarenko@users.noreply.github.com>
2023-08-08 14:15:55 +0000

    Deploying to gh-pages from @ AndreyPysarenko/goit-js-hw-11@bf3ad95d637ba86c6e481db189883dfdd2e4c251 🚀

--- index.80b7b3ab.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".gallery").firstElementChild.getBoundingClientRect().height;window.scrollBy({top:2*e,behavior:"smooth"})}();
-//# sourceMappingURL=index.80b7b3ab.js.map
--- index.963b441d.js
+++
@@ -1,2 +0,0 @@
-const{height:e}=document.querySelector(".gallery").firstElementChild.getBoundingClientRect();window.scrollBy({top:2*e,behavior:"smooth"});
-//# sourceMappingURL=index.963b441d.js.map
